a440b7b0dbc3e372723cd529b0c76e645934dd6e
Samuel Knight <robogamingbysam@icloud.com>
2025-11-22 00:11:56 -0500

    Optimize range() function

--- src/elegance.ts
+++ src/elegance.ts
@@ -1,7 +1,13 @@
+import { assert } from "@std/assert/assert";
+
 /**
  * From Stack Overflow, a more elegant way to loop for n times, inspired by the pythonic syntax.
  *
- * for (const i of range(5)) {}
+ * for (const i of range(5)) { console.log(i) } // 0, 1, 2, 3, 4
+ *
+ * @param start Start of range, inclusive; will be end of range if only one parameter is provided
+ * @param stop End of range, exclusive
+ * @param step Will go from start to stop in steps of size step, may be negative if stop < start
  */
 export function* range(start: number, stop: number | undefined = undefined, step = 1) {
   if (stop == undefined) {
@@ -10,7 +16,17 @@ export function* range(start: number, stop: number | undefined = undefined, step
     start = 0;
   }
 
-  for (let i = start; step > 0 ? i < stop : i > stop; i += step) {
-    yield i;
+  if (step > 0) {
+    assert(stop > start);
+    // Counting forward
+    for (let i = start; i < stop; i += step) {
+      yield i;
+    }
+  } else {
+    assert(stop < start);
+    // Counting backward
+    for (let i = start; i > stop; i += step) {
+      yield i;
+    }
   }
 }

76e2acd3eea9b5e38bce77db10d730474b8ee2dc
Samuel Knight <robogamingbysam@icloud.com>
2025-11-21 23:47:05 -0500

    Test accepting an item

--- test/ring_queue_test.ts
+++ test/ring_queue_test.ts
@@ -8,4 +8,10 @@ describe("Ring Queue", () => {
     assert(rq.isEmpty);
     assertEquals(rq.size(), 0);
   });
+
+  it("should accept an item", () => {
+    const rq = new RingQueue<string>();
+
+    rq.enqueue("Item One");
+  });
 });

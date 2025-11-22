c277adca73fba0ca04eb0a337e78b46ff7cb7dc1
Samuel Knight <robogamingbysam@icloud.com>
2025-11-21 23:41:50 -0500

    Formatting, range() function

--- main.ts
+++ main.ts
@@ -1,3 +1,1 @@
-
 console.log("Starting crawler & indexer.");
-
---
+++ src/elegance.ts
@@ -0,0 +1,16 @@
+/**
+ * From Stack Overflow, a more elegant way to loop for n times, inspired by the pythonic syntax.
+ *
+ * for (const i of range(5)) {}
+ */
+export function* range(start: number, stop: number | undefined, step = 1) {
+  if (stop == undefined) {
+    // The first parameter is implicitly stop if only one parameter is given.
+    stop = start;
+    start = 0;
+  }
+
+  for (let i = start; step > 0 ? i < stop : i > stop; i += step) {
+    yield i;
+  }
+}

e74516035d19518063eff3da799cdb218f472e1b
Samuel Knight <robogamingbysam@icloud.com>
2025-11-21 23:46:06 -0500

    Implement size()/isEmpty()

--- src/ring_queue.ts
+++ src/ring_queue.ts
@@ -1,44 +1,48 @@
-export class RingQueue<T> {
-    items: [T] | [] = [];
-    startIndex: number = 0;
-    endIndex: number = 0;
+export class RingQueue<E> {
+  items: [E | undefined] = new Array(10) as [undefined];
+  startIndex: number = 0; // inclusive
+  endIndex: number = 0; // exclusive
 
-    /**
-     * isEmpty
-     */
-    public isEmpty(): boolean {
-        throw new Error("Not implemented");
-    }
+  /**
+   * isEmpty
+   */
+  public isEmpty(): boolean {
+    return this.size() == 0;
+  }
 
-    /**
-     * size
-     */
-    public size(): number {
-        throw new Error("Not implemented");
+  /**
+   * size
+   */
+  public size(): number {
+    if (this.startIndex >= this.endIndex) {
+      return this.endIndex - this.startIndex;
+    } else {
+      return this.items.length - (this.startIndex - this.endIndex);
     }
+  }
 
-    /**
-     * enqueue
-     */
-    public enqueue() {
-        throw new Error("Not implemented");
-    }
+  /**
+   * enqueue
+   */
+  public enqueue(item: E) {
+    throw new Error("Not implemented");
+  }
 
-    /**
-     * dequeue
-     */
-    public dequeue(): T {
-        throw new Error("Not implemented");
-    }
+  /**
+   * dequeue
+   */
+  public dequeue(): E {
+    throw new Error("Not implemented");
+  }
 
-    /**
-     * clear
-     */
-    public clear() {
-        throw new Error("Not implemented");
-    }
+  /**
+   * clear
+   */
+  public clear() {
+    throw new Error("Not implemented");
+  }
 
-    private doubleSizeIfFull(): boolean {
-        throw new Error("Not implemented");
-    }
+  private doubleSizeIfFull(): boolean {
+    throw new Error("Not implemented");
+  }
 }
--- test/ring_queue_test.ts
+++ test/ring_queue_test.ts
@@ -1,13 +1,11 @@
-import { assert } from "@std/assert";
+import { assert, assertEquals } from "@std/assert";
 import { describe, it } from "@std/testing/bdd";
 import { RingQueue } from "../src/ring_queue.ts";
 
 describe("Ring Queue", () => {
-  describe("When the queue is empty", () => {
-    it("should report emptiness", () => {
-        const rq = new RingQueue<string>();
-        assert(rq.isEmpty());
-        assert(rq.size() == 0);
-    });
+  it("should report emptiness", () => {
+    const rq = new RingQueue<string>();
+    assert(rq.isEmpty);
+    assertEquals(rq.size(), 0);
   });
 });
